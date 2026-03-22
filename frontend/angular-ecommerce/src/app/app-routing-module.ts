8317cdbd599a709bdf5e0c441c64fc50abe275d0
Matthew Coscia <coscia.matthew@gmail.com>
2026-03-22 15:55:35 -0400

    Add Display Products by Category

--- frontend/angular-ecommerce/src/app/app-routing-module.ts
+++ frontend/angular-ecommerce/src/app/app-routing-module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
 
-const routes: Routes = [];
+// Note: The order of the routes listed matters. First match wins!
+const routes: Routes = [ 
+  {path: 'category/:id', component: ProductList},
+  {path: 'category', component: ProductList},
+  {path: 'products', component: ProductList},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+ ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.ts
@@ -1,27 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: false,
   templateUrl: './product-grid.html',
   styleUrl: './product-list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class ProductList implements OnInit {
-  
-  constructor(private productService: ProductService) {}
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
+  
+  constructor(private productService: ProductService, 
+              private route: ActivatedRoute,
+              private cdr: ChangeDetectorRef) {}
+
   
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+        this.listProducts();
+      });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // Check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the 'id' param string and convert to a number
+      // '+' converts to number
+      //'!' tells the compiler the object is not null
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
+
+        this.cdr.markForCheck();
       }
     )
   }
